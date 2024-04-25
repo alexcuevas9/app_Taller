eaee29be299aa4fd9293262e24fccc5c37690c11
unknown <carcastro@uniboyaca.edu.co>
2024-04-25 16:13:10 -0500

    Se agregaron modificaciones

--- EjercicioNª4/js/juego.js
+++ EjercicioNª4/js/juego.js
@@ -1,72 +1,68 @@
+String.prototype.replaceAt = function (index, character) { return this.substring(0, index) + character + this.substring(index + character.length); }
+
+//conjunto de palabras para el juego
+const button = document.querySelector('#calcular');//asignando button a boton calcular
 const palabras = ['javascript', 'html', 'css', 'python', 'java'];
+//palabra randon
+let palabra = palabras[Math.floor(Math.random() * palabras.length)];
+//remplazar las palabras por guion y espacio
+let palabraConGuiones = palabra.replace(/./g, "_ ");
 
-// Elige una palabra aleatoria del array 'palabras'
-let palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
+let contadorFallos = 0;
+//<p> id output sera reemplazado por "palabraConGuiones"
+document.querySelector('#output').innerHTML = palabraConGuiones;
 
-// Define el número máximo de intentos
-let intentos = 6;
-
-// Crea una cadena de guiones bajos (_) con la misma longitud que la palabra secreta
-let palabraMostrada = '_'.repeat(palabraSecreta.length);
-
-// Muestra la palabra oculta y el número de intentos restantes en el HTML
-document.getElementById('palabra').innerText = palabraMostrada;
-document.getElementById('intentos').innerText = `Intentos restantes: ${intentos}`;
-
-// Función para jugar, se llama cuando el jugador presiona el botón de "Jugar"
-function jugar() {
-    // Obtiene la letra ingresada por el jugador y la convierte a minúsculas
-    const letra = document.getElementById('letra').value.toLowerCase();
-    
-    // Verifica si la entrada es una letra válida
-    if (letra.length !== 1 || !(/[a-z]/.test(letra))) {
-        alert('Ingresa una letra válida.');
-        return;
-    }
-
-    // Verifica si la letra ingresada está en la palabra secreta
-    if (palabraSecreta.includes(letra)) {
-        // Reemplaza los guiones bajos (_) en la palabra mostrada por la letra correcta
-        for (let i = 0; i < palabraSecreta.length; i++) {
-            if (palabraSecreta[i] === letra) {
-                palabraMostrada = palabraMostrada.substr(0, i) + letra + palabraMostrada.substr(i + 1);
-            }
+button.addEventListener('click', () => {
+    //obtener el valor de la letra ingresada en id letra y guardarlo en la const letra.
+    let letra = document.querySelector('#letra').value;
+    letra = letra.toLowerCase();
+    let haFallado = true;
+    for (const i in palabra) {
+        //si encuentra la letra, reemplazara el guión+espacio por la letra que encontro en el espacio en que lo encontro.
+        if (letra == palabra[i]) {
+            palabraConGuiones = palabraConGuiones.replaceAt(i * 2, letra);
+            haFallado = false;
         }
-        // Muestra la palabra actualizada en el HTML
-        document.getElementById('palabra').innerText = palabraMostrada;
+    }
+    /* condición de ganador o perdedor */
+    if (haFallado) {
+        contadorFallos++;
+        document.querySelector('#ahorcado').style.backgroundPosition = -(180 * contadorFallos) + 'px 0px';
+        if (contadorFallos > 2) {
+            document.querySelector('#ahorcado').style.backgroundPosition = -(198 * contadorFallos) + 'px 220px';
+        }
+        if (contadorFallos >= 5) {
+            document.querySelector('#resultado').innerHTML = "Perdiste, has sido ahorcado.";
+            document.getElementById('resultado').style.backgroundColor = "#740707";
+            button.disabled = true;
+            setTimeout(resetGame, 1200); // Llama a la función para reiniciar el juego después de 1.2 segundo
+        }
     } else {
-        // Reduce el número de intentos restantes si la letra no está en la palabra secreta
-        intentos--;
-        // Actualiza el contador de intentos restantes en el HTML
-        document.getElementById('intentos').innerText = `Intentos restantes: ${intentos}`;
+        if (palabraConGuiones.indexOf('_') < 0) {
+            document.querySelector('#resultado').innerHTML = "¡¡Eres un Ganador!!";
+            document.getElementById('resultado').style.backgroundColor = "#0c5ca7";
+            setTimeout(resetGame, 1200); // Llama a la función para reiniciar el juego después de 1.2 segundo
+            button.disabled = true;
+        }
     }
 
-    // Verifica si el jugador ha perdido
-    if (intentos === 0) {
-        alert('¡Has perdido! La palabra era: ' + palabraSecreta);
-        // Reinicia el juego
-        reiniciarJuego();
-    } 
-    // Verifica si el jugador ha adivinado todas las letras de la palabra
-    else if (!palabraMostrada.includes('_')) {
-        alert('¡Felicidades! Has adivinado la palabra.');
-        // Reinicia el juego
-        reiniciarJuego();
-    }
+    /* document.querySelector('#ahorcado').style.backgroundPosition= -(100*contadorFallos)+'px'; */
+    /* reemplazar los datos de id output por la variable palabra con guiones */
+    document.querySelector('#output').innerHTML = palabraConGuiones;
+    limpiarInput(); // Llama a la función para limpiar el campo de entrada
+});
+// Función para limpiar el campo de entrada
+function limpiarInput() {
+    document.querySelector('#letra').value = ''; // Establece el valor del campo de entrada a una cadena vacía
 }
-
-// Función para reiniciar el juego
-function reiniciarJuego() {
-    // Elige una nueva palabra secreta aleatoria
-    palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
-    // Restablece el número de intentos
-    intentos = 6;
-    // Crea una nueva palabra mostrada con guiones bajos (_) para la nueva palabra secreta
-    palabraMostrada = '_'.repeat(palabraSecreta.length);
-    // Muestra la nueva palabra oculta y el número de intentos restantes en el HTML
-    document.getElementById('palabra').innerText = palabraMostrada;
-    document.getElementById('intentos').innerText = `Intentos restantes: ${intentos}`;
-    // Limpia el campo de entrada de letras
-    document.getElementById('letra').value = '';
+function resetGame() { // Función para reiniciar el juego
+    document.querySelector('#letra').value = ''; // Limpia el campo de entrada
+    palabra = palabras[Math.floor(Math.random() * palabras.length)]; // Selecciona una nueva palabra
+    palabraConGuiones = palabra.replace(/./g, "_ "); // Reinicia la palabra con guiones
+    contadorFallos = 0; // Reinicia el contador de fallos
+    document.querySelector('#ahorcado').style.backgroundPosition = '0px 0px'; // Reinicia la posición del ahorcado
+    document.querySelector('#output').innerHTML = palabraConGuiones; // Actualiza la palabra con guiones en la pantalla
+    document.querySelector('#resultado').innerHTML = ''; // Limpia el resultado anterior
+    document.getElementById('resultado').style.backgroundColor = 'initial'; // Reinicia el color de fondo del resultado
+    document.querySelector('#calcular').disabled = false; // Habilita el botón calcular
 }
-
--- EjercicioNº2/java/script.js
+++ EjercicioNº2/java/script.js
@@ -1,50 +1,74 @@
-const availableCards = ['A', 'K', 'Q', 'J']; 
-let cards = []; //Es un array vacío donde se almacenarán las cartas del juego.
-let selectedCards = []; //almacena temporalmente las cartas seleccionadas por el jugador.
-let valuesUsed = []; //almacena los valores utilizados para las cartas.
-let currentMove = 0; //variable que llevará la cuenta del número de movimientos actuales.
-let currentAttempts = 0; //Variable que llevará la cuenta del número de intentos actuales.
+// Array que contiene las cartas disponibles para el juego: A, K, Q, J
+const availableCards = ['A', 'K', 'Q', 'J'];
 
+// Array vacío donde se almacenarán las cartas del juego
+let cards = [];
+
+// Array que almacena temporalmente las cartas seleccionadas por el jugador
+let selectedCards = [];
+
+// Array que almacena los valores utilizados para las cartas
+let valuesUsed = [];
+
+// Variable que lleva la cuenta del número de movimientos actuales
+let currentMove = 0;
+
+// Variable que lleva la cuenta del número de intentos actuales
+let currentAttempts = 0;
+
+// Cadena de texto que contiene el HTML para una carta en el juego
 let cardTemplate = '<div class="card"><div class="back"></div><div class="face"></div></div>';
-//cadena de texto que contiene el HTML para una carta en el juego.
 
+// Evento que inicia el juego cuando se hace clic en el botón 'startGame'
 document.getElementById('startGame').addEventListener('click', startGame);
-document.getElementById('restartGame').addEventListener('click', restartGame); 
-//eventospara ejecutar ciertas funciones cuando se hace clic en los elementos
 
+// Evento que reinicia el juego cuando se hace clic en el botón 'restartGame'
+document.getElementById('restartGame').addEventListener('click', restartGame);
+
+// Función que inicia el juego
 function startGame() {
+    // Obtiene la cantidad de cartas ingresada por el usuario
     const cardCount = parseInt(document.getElementById('cardCount').value);
+    // Verifica si la cantidad de cartas está en el rango válido (entre 2 y 52)
     if (cardCount >= 2 && cardCount <= 52) {
+        // Calcula el total de cartas necesarias para el juego
         totalCards = cardCount * 2;
+        // Reinicia el juego
         resetGame();
-        document.getElementById('instruction').style.display = 'none'; // Oculta el enunciado al iniciar el juego
-        document.getElementById('startGame').style.display = 'none'; // Oculta el botón de inicio
-        document.getElementById('restartGame').style.display = 'block'; // Muestra el botón de reiniciar juego
+        // Oculta el enunciado al iniciar el juego
+        document.getElementById('instruction').style.display = 'none';
+        // Oculta el botón de inicio
+        document.getElementById('startGame').style.display = 'none';
+        // Muestra el botón de reiniciar juego
+        document.getElementById('restartGame').style.display = 'block';
+        // Configura el juego
         setupGame();
     } else {
+        // Muestra un mensaje de alerta si el número ingresado no es válido
         alert('Por favor, ingresa un número válido entre 2 y 52.');
     }
 }
 
+// Función que reinicia el juego
 function resetGame() {
-   currentMove = 0;
-   currentAttempts = 0;
-   selectedCards = [];
-   valuesUsed = [];
-   document.querySelector('#game').innerHTML = '';
-   document.querySelector('#stats').innerHTML = '0 intentos';
-
-   // Remover la clase 'active' de todas las cartas
-   document.querySelectorAll('.card').forEach(card => {
-       card.classList.remove('active');
-   });
-
-   // Limpiar el array de cartas
-   cards = [];
+    // Reinicia las variables y elementos del juego
+    currentMove = 0;
+    currentAttempts = 0;
+    selectedCards = [];
+    valuesUsed = [];
+    document.querySelector('#game').innerHTML = '';
+    document.querySelector('#stats').innerHTML = '0 intentos';
+    // Remueve la clase 'active' de todas las cartas
+    document.querySelectorAll('.card').forEach(card => {
+        card.classList.remove('active');
+    });
+    // Limpia el array de cartas
+    cards = [];
 }
 
-
+// Función que configura el juego
 function setupGame() {
+    // Itera para crear las cartas y configurar los eventos de clic
     for (let i = 0; i < totalCards; i++) {
         let div = document.createElement('div');
         div.innerHTML = cardTemplate;
@@ -56,20 +80,70 @@ function setupGame() {
     }
 }
 
+// Función que genera valores aleatorios para las cartas
+function randomValue() {
+    let rnd = Math.floor(Math.random() * totalCards * 0.5);
+    let values = valuesUsed.filter(value => value === rnd);
+    if (values.length < 2) {
+        valuesUsed.push(rnd);
+    } else {
+        randomValue();
+    }
+}
+
+// Función que obtiene el valor de la carta en función del índice del valor utilizado
+function getFaceValue(value) {
+    let rtn = value;
+    if (value < availableCards.length) {
+        rtn = availableCards[value];
+    }
+    return rtn;
+}
+
+// Función que reinicia el juego
+function restartGame() {
+    resetGame();
+    // Muestra el enunciado al reiniciar
+    document.getElementById('instruction').style.display = 'block';
+    // Muestra el botón de inicio al reiniciar
+    document.getElementById('startGame').style.display = 'block';
+    // Oculta el botón de reiniciar juego al reiniciar
+    document.getElementById('restartGame').style.display = 'none';
+}
+
+// Función que verifica si todas las cartas están emparejadas y muestra un mensaje de victoria
+function checkVictory() {
+    let matched = 0;
+    document.querySelectorAll('.card').forEach(card => {
+        if (card.classList.contains('active')) {
+            matched++;
+        }
+    });
+    if (matched === totalCards) {
+        // Muestra un mensaje de victoria si todas las cartas están emparejadas
+        alert('¡Felicidades! ¡Has encontrado todas las parejas!');
+    }
+}
+
+// Función que se activa cuando se hace clic en una carta
 function activate(e) {
     if (currentMove < 2) {
         if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
+            // Añade la clase 'active' a la carta seleccionada
             e.target.classList.add('active');
             selectedCards.push(e.target);
-
             if (++currentMove == 2) {
+                // Incrementa el número de intentos
                 currentAttempts++;
                 document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
-
                 if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
+                    // Si las cartas seleccionadas son iguales, reinicia el conteo de movimientos
                     selectedCards = [];
                     currentMove = 0;
+                    // Verifica si todas las cartas están emparejadas
+                    checkVictory();
                 } else {
+                    // Si las cartas seleccionadas no son iguales, las desactiva después de un tiempo
                     setTimeout(() => {
                         selectedCards[0].classList.remove('active');
                         selectedCards[1].classList.remove('active');
@@ -82,71 +156,6 @@ function activate(e) {
     }
 }
 
-function randomValue() {
-    let rnd = Math.floor(Math.random() * totalCards * 0.5);
-    let values = valuesUsed.filter(value => value === rnd);
-    if (values.length < 2) {
-        valuesUsed.push(rnd);
-    } else {
-        randomValue();
-    }
-}
-
-function getFaceValue(value) {
-    let rtn = value;
-    if (value < availableCards.length) {
-        rtn = availableCards[value];
-    }
-    return rtn;
-}
-
-function restartGame() {
-    resetGame();
-    document.getElementById('instruction').style.display = 'block'; // Muestra el enunciado al reiniciar
-    document.getElementById('startGame').style.display = 'block'; // Muestra el botón de inicio al reiniciar
-    document.getElementById('restartGame').style.display = 'none'; // Oculta el botón de reiniciar juego al reiniciar
-}
-
-function checkVictory() {
-   let matched = 0;
-   document.querySelectorAll('.card').forEach(card => {
-       if (card.classList.contains('active')) {
-           matched++;
-       }
-   });
-
-   if (matched === totalCards) {
-       // Todas las cartas están emparejadas, mostrar mensaje de victoria
-       alert('¡Felicidades! ¡Has encontrado todas las parejas!');
-   }
-}
-
-function activate(e) {
-   if (currentMove < 2) {
-       if ((!selectedCards[0] || selectedCards[0] !== e.target) && !e.target.classList.contains('active')) {
-           e.target.classList.add('active');
-           selectedCards.push(e.target);
-
-           if (++currentMove == 2) {
-               currentAttempts++;
-               document.querySelector('#stats').innerHTML = currentAttempts + ' intentos';
-
-               if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML) {
-                   selectedCards = [];
-                   currentMove = 0;
-                   checkVictory(); // Verificar si todas las cartas están emparejadas
-               } else {
-                   setTimeout(() => {
-                       selectedCards[0].classList.remove('active');
-                       selectedCards[1].classList.remove('active');
-                       selectedCards = [];
-                       currentMove = 0;
-                   }, 600);
-               }
-           }
-       }
-   }
-}
 
 
 
